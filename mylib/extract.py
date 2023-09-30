616eac7ff72096e5971028478075b141910dc5ff
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-09-30 00:00:23 +0000

    add new

--- mylib/extract.py
+++ mylib/extract.py
@@ -4,7 +4,8 @@ extract a dataset from URL
 import requests
 url = "https://raw.githubusercontent.com/MainakRepositor/Datasets/master/Air%20Quality/real_2016_air.csv"
 filepath = "./data/real_2016_air.csv"
-def extract_dataSource(url,filepath):
+def extract_dataSource(url = "https://raw.githubusercontent.com/MainakRepositor/Datasets/master/Air%20Quality/real_2016_air.csv",
+                       filepath = "./data/real_2016_air.csv"):
     ''' extract data from url to a filepath '''
     with requests.get(url) as r:
         with open(filepath, 'wb') as f:
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -5,12 +5,19 @@ import sqlite3
 import csv
 import os
 
+dataset = "/workspace/mini_project_5/data/real_2016_air.csv"
 # load csv file and insert into a sqlite3 database
-def load(dataset):
+def load(dataset = "/workspace/mini_project_5/data/real_2016_air.csv"):
 
     # print the full working directory
     print(os.getcwd())
-    
-
-
-    return 
+    payload = csv.reader(open(dataset,newline=""),delimiter=",")
+    conn = sqlite3.connect("real_2016_air.db")
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS real_2016_air")
+    c.execute("CREATE TABLE real_2016_air (id,T,TM,Tm,SLP,H,VV,V,VM,PM 2.5)")
+    # insert values
+    c.executemany("INSERT INTO real_2016_air VALUES(?,?,?,?,?,?,?,?,?,?)",payload)
+    conn.commit()
+    conn.close()
+    return "real_2016_air.db"
