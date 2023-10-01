d36d010bea1a8fd9fbf56d4c71c52721f38486d4
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-10-01 17:08:49 +0000

    add all

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Main cli or app entry point
 """
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import (querycreate,queryRead,queryUpdate,queryDelete)
+from mylib.query import querycreate, queryRead, queryUpdate, queryDelete
 
 # Extract
 extract()
@@ -17,16 +17,18 @@ queryRead()
 queryUpdate()
 queryDelete()
 
+
 def main_res():
-    results ={
-            "extract_to":extract(),
-            "transform_db":load(),
-            "create":querycreate(),
-            "read":queryRead(),
-            "update":queryUpdate(),
-            "delete":queryDelete()
-            }
+    results = {
+        "extract_to": extract(),
+        "transform_db": load(),
+        "create": querycreate(),
+        "read": queryRead(),
+        "update": queryUpdate(),
+        "delete": queryDelete(),
+    }
 
     return results
+
 
 main_res()
--- test_main.py
+++ test_main.py
@@ -3,8 +3,10 @@ Test goes here
 """
 from main import main_res
 
+
 def test_func():
     return main_res()
+
 
 if __name__ == "__main__":
     assert test_func()["extract_to"] == "./data/rainfall.csv"
@@ -13,6 +15,3 @@ if __name__ == "__main__":
     assert test_func()["read"] == "Read Success"
     assert test_func()["update"] == "Update Success"
     assert test_func()["delete"] == "Delete Success"
-  
-  
-
