a23ef1f5ae511dd15cf4fff92bc86fc31cf7294f
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-10-02 12:34:19 -0400

    changes

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,20 +1,21 @@
-'''
+"""
 Transforms and loads data to local SQlite3 database
-'''
+"""
 import sqlite3
 import csv
 import os
 
-def load(dataset = "data/rainfall.csv"):
 
+def load(dataset="../data/rainfall.csv"):
     # print the full working directory
-    #print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=""),delimiter=",")
+    # print(os.getcwd())
+    payload = csv.reader(open(dataset, newline=""), delimiter=",")
     conn = sqlite3.connect("rainfall.db")
     c = conn.cursor()
     # create new table for the database rainfall
     c.execute("DROP TABLE IF EXISTS rainfall")
-    c.execute('''
+    c.execute(
+        """
               CREATE TABLE rainfall (
               id INTEGER PRIMARY KEY AUTOINCREMENT,
               Date DATE,
@@ -22,16 +23,21 @@ def load(dataset = "data/rainfall.csv"):
               CHOLAVARAM FLOAT,
               REDHILLS FLOAT,
               CHEMBARAMBAKKAM FLAOT)
-              ''')
+              """
+    )
     # insert values
-    c.executemany('''
+    c.executemany(
+        """
                   INSERT INTO rainfall 
                   (Date,POONDI,CHOLAVARAM,REDHILLS,CHEMBARAMBAKKAM)
                   VALUES(?,?,?,?,?)
-                  ''', payload)
+                  """,
+        payload,
+    )
     conn.commit()
     conn.close()
     return "rainfall.db"
+
 
 if __name__ == "__main__":
     load()
